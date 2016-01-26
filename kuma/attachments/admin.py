9ad37e2e3625367cbc3142e962126720078bfdf1
Jannis Leidel <jannis@leidel.info>
2016-01-26 11:11:31 +0100

    Bug 1110801 - Remove unused attachment file listing view in favor of improved admin UI.

--- kuma/attachments/admin.py
+++ kuma/attachments/admin.py
@@ -3,11 +3,21 @@ from django.contrib import admin
 from .models import Attachment, AttachmentRevision
 
 
+class AttachmentRevisionInline(admin.StackedInline):
+    model = AttachmentRevision
+    extra = 1
+    can_delete = False
+
+
 class AttachmentAdmin(admin.ModelAdmin):
     fields = ('current_revision', 'mindtouch_attachment_id')
     list_display = ('title', 'slug', 'modified', 'mindtouch_attachment_id')
     ordering = ('title',)
     search_fields = ('title',)
+    raw_id_fields = ['current_revision']
+    inlines = [
+        AttachmentRevisionInline,
+    ]
 
 
 class AttachmentRevisionAdmin(admin.ModelAdmin):
@@ -16,7 +26,7 @@ class AttachmentRevisionAdmin(admin.ModelAdmin):
     list_display = ('title', 'created')
     ordering = ('-created', 'title')
     search_fields = ('title', 'description')
-
+    raw_id_fields = ['attachment']
 
 admin.site.register(Attachment, AttachmentAdmin)
 admin.site.register(AttachmentRevision, AttachmentRevisionAdmin)
